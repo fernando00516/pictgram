74a64f2f1559ffaad8017c2230cdb320c5d9a164
shiomifernando <tls0516@icloud.com>
2019-03-05 23:13:22 +0900

    rails repository

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :password, length: { in: 8..32 },
+  format: {with: /A[a-zA-Z0-9]+z/}
+
+  has_secure_password
+end

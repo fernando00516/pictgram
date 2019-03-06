7216f8d15d71b15ed463ccece527cb8f0a675899
shiomifernando <tls0516@icloud.com>
2019-03-06 20:49:26 +0900

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
-  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-  validates :password, length: { in: 8..32 },
-  format: {with: /A[a-zA-Z0-9]+z/}
+  validates :email, presence: true, format: {with: /\w+@\w+\.\w+/}
+  validates :password,format: {with: /A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}z/i}
 
   has_secure_password
 end

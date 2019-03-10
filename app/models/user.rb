323d41a053b93678ffd530ee02351163f4e8eadf
shiomifernando <tls0516@icloud.com>
2019-03-10 22:35:08 +0900

    commit_topics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true, format: {with: /\w+@\w+\.\w+/}
-  validates :password,format: {with: /A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}z/i}
+  validates :password,format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i}
 
   has_secure_password
+
+  has_many :topics
 end
